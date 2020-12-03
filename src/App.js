38c2a3ad1c5c96795076ec2009f4815495854783
MonnaB4u <Moheuddin.monnab4u@gmail.com>
2020-12-03 10:20:41 +0800

    x-gym

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "./App.css";
+import Class from "./Components/Classes/Class/Class";
+import Classes from "./Components/Classes/Classes";
+import Home from "./Components/HomePage/Home";
+import Pricing from "./Components/Pricing/Pricing";
+import MemberShip from "./Components/Bank/MembershipForm/MembershipForm";
+import BankPayment from "./Components/Bank/BankPayment/BankPayment";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/home" component={Home}>
+            <Home />
+          </Route>
+          <Route path="/registry">
+            <MemberShip></MemberShip>
+          </Route>
+
+          <Route path="/payment">
+            <BankPayment></BankPayment>
+          </Route>
+          <Route path="/classes/:id" children={Class}></Route>
+          <Route path="/classes" component={Classes}>
+            <Classes></Classes>
+          </Route>
+          <Route path="/pricing" component={Pricing}>
+            <Pricing></Pricing>
+          </Route>
+          <Route exact path="/home" component={Home}>
+            <Home></Home>
+          </Route>
+          <Route exact path="/" component={Home}>
+            <Home></Home>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
